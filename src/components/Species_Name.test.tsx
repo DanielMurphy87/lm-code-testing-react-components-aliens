6db488d2f6c711bf9820f325f8e331bc3373282e
DanielMurphy87 <84238673+DanielMurphy87@users.noreply.github.com>
2023-03-15 23:19:16 +0200

    Updated some tests

--- src/components/Species_Name.test.tsx
+++ src/components/Species_Name.test.tsx
@@ -2,6 +2,7 @@
 
 import { render, fireEvent, screen } from '@testing-library/react';
 import SpeciesName from './Species_Name';
+import user from '@testing-library/user-event';
 
 test('renders label and input element', () => {
     render(<SpeciesName speciesName={''} onChangeSpeciesName={() => { }} />);
@@ -25,4 +26,29 @@ test('renders input element with correct value', () => {
     render(<SpeciesName speciesName={'Earth'} onChangeSpeciesName={() => { }} />);
     const input = screen.getByDisplayValue('Earth');
     expect(input).toHaveValue('Earth');
+});
+
+test('displays error message when species name is empty', () => {
+    render(<SpeciesName speciesName={''} onChangeSpeciesName={() => { }} />);
+    const inputElement = screen.getByLabelText(/species name/i);
+    fireEvent.change(inputElement, { target: { value: '' } });
+    const errorElement = screen.getByText(/species name cannot be empty/i);
+    expect(errorElement).toBeInTheDocument();
+});
+
+test('should validate the species name correctly', async () => {
+    render(<SpeciesName speciesName="" onChangeSpeciesName={() => { }} />);
+    const speciesNameInput = screen.getByLabelText('Species Name:');
+    user.type(speciesNameInput, '{backspace}');
+    await new Promise(resolve => setTimeout(resolve, 0));
+    expect(screen.getByText('Species name cannot be empty')).toBeInTheDocument();
+    user.type(speciesNameInput, 'a'.repeat(21));
+    await new Promise(resolve => setTimeout(resolve, 0));
+    expect(screen.getByText('Species name cannot be longer than 20 characters')).toBeInTheDocument();
+});
+
+test('does not display error message when species name is valid', () => {
+    render(<SpeciesName speciesName={'Earth'} onChangeSpeciesName={() => { }} />);
+    const errorElement = screen.queryByText(/species name cannot be empty/i);
+    expect(errorElement).not.toBeInTheDocument();
 });
